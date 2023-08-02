79eba2ed26b87c9982faa1e11975ef59203cf3ed
ArcticPeAr <54914210+ArcticPeAr@users.noreply.github.com>
2023-08-02 15:11:22 +0200

    Find genes in Tippy with gene name conversion

--- FindGenesInTippyTop.R
+++ FindGenesInTippyTop.R
@@ -4,20 +4,28 @@ df <- read_feather("/home/petear/MEGA/TormodGroup/InputData/TippyTopGeneDF_ALL.f
 df2 <- as.data.frame(df)
 
 #Get integers from a text file
-path <- "TestGenesEntrez"
-my_data <- readLines(path)
-my_data <- as.integer(my_data)
+geneVector <- c("AXL","IL18","IL1B","ADAM10","PSEN1","IFITM3","BACE1","BIN1","CLU","LRP3","LILRB2","TREM2","SQSTM1")
 
-#start subset
-keep <- df2$ENTREZID %in% my_data
-my_subset <- df2[keep, ]
 
 #convert entrezid to symbol
 library(org.Hs.eg.db)
-my_subset$SYMBOL <- mapIds(org.Hs.eg.db, keys = my_subset$ENTREZID, column = "SYMBOL", keytype = "ENTREZID")
+entrez_ids <- select(org.Hs.eg.db, keys = geneVector, columns = "ENTREZID", keytype = "SYMBOL")
+#remove NA
+valid_entrez_ids <- na.omit(entrez_ids$ENTREZID)
+
+# Assuming df2 is your data frame with the ENTREZID column
+filtered_df2 <- subset(df2, ENTREZID %in% valid_entrez_ids)
+
+merged_df <- merge(filtered_df2, entrez_ids, by.x = "ENTREZID", by.y = "ENTREZID")
+write_xlsx(merged_df, path = "filtered_df2.xlsx")
+
+#save as ods
+library(readODS)
+write_ods(merged_df, path = "TestGenesEntrez.ods")
 
 
 #save as csv
-write.csv(my_subset, file = "TestGenesEntrez.csv", row.names = FALSE)
+write.csv(merged_df, file = "TestGenesEntrez.csv", row.names = FALSE)
 
 #
+library(writexl)
